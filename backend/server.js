5a25a423d80e73a78900d58f796a936f57197ea4
antonioplese <antonioplese0@gmail.com>
2024-10-22 16:37:35 +0100

    Server setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+const corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Inkstitution!" });
+  });
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on port ${PORT}`);
+});
+
