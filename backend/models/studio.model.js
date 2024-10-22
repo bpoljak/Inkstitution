2474cf789bed6c9163b7298fa6f201a691c324fd
bpoljak <borna.poljak12@gmail.com>
2024-10-22 16:59:05 +0200

    Created construictors for studios and aftercare products

---
+++ backend/models/studio.model.js
@@ -0,0 +1,11 @@
+const sql = require("./db.js");
+
+const Studio = function(studio) {
+    this.studioId = studio.studioId
+    this.studioName = studio.studioName;
+    this.studioAddres = studio.studioAddres;
+    this.studioWorkingHours = studio.studioWorkingHours;
+    this.studioEmail = studio.studioEmail;
+    this.studioAccountName =studio.studioAccountName;
+    this.studioPassword = studio.studioPassword
+}
