0f9bac917e0562a0f094f0a8767777798b0a594e
antonioplese <antonioplese0@gmail.com>
2024-10-22 16:59:41 +0100

    Created constructors for appointments,artists and userimages

---
+++ backend/models/userimage.model.js
@@ -0,0 +1,8 @@
+const sql = require("./db.js");
+
+const UserImage = function(userImage) {
+    this.userImageId = userImage.userImageId;
+    this.userImageDescription = userImage.userImageDescription;
+    this.userImageLink = userImage.userImageLink;
+    this.profileImageLink = userImage.profileImageLink;
+  };
