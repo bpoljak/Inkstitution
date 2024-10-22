0c77c5dd1baff3c74121058c2c00be5a5268df91
Dinotije 23 <ja@dino.com>
2024-10-22 17:02:52 +0200

    Created constructors for User and UserImages

---
+++ backend/models/user.model.js
@@ -0,0 +1,11 @@
+const sql = require("./db.js");
+
+// constructor
+const User = function(User) {
+  this.userId = userId.title;
+  this.userFirstName = User.UserFirstName;
+  this.userLastName = User.UserLastName;
+  this.userEmail = User.UserEmail
+  this.userAccountName = User.UserAccountName
+  this.userPassword = User.UserPassword
+};
