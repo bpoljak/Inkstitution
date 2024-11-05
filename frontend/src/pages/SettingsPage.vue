0a7ed28e47a76a70a82d63cf1c1d2069a530aa12
Dinotije 23 <ja@dino.com>
2024-11-05 14:10:46 +0100

    Added settings page

--- frontend/src/pages/SettingsPage.vue
+++ frontend/src/pages/SettingsPage.vue
@@ -1,11 +1,57 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page class="q-pa-md">
+    <h1>Postavke</h1>
+    <q-card-section class="q-pt-none">
+      <q-toggle
+        v-model="darkMode"
+        :label="$t('darkMode')"
+        @update:model-value="changeDarkMode"
+      />
+
+      <q-select
+        v-model="locale"
+        :options="languages"
+        dense
+        emit-value
+        map-options
+        options-dense
+        @update:model-value="changeLanguage"
+        :label="$t('language')"
+      />
+    </q-card-section>
   </q-page>
 </template>
 
-<script>
-export default {
-  // name: 'PageName',
-}
+<script setup>
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+import { useI18n } from "vue-i18n";
+
+const $q = useQuasar();
+const { locale } = useI18n({ useScope: "global" });
+
+const darkMode = ref($q.dark.isActive);
+const changeDarkMode = () => {
+  $q.dark.toggle();
+  $q.localStorage.set("isDarkActive", $q.dark.isActive);
+};
+
+const currentLang = ref("hr");
+
+const languages = [
+  { value: "en", label: "English" },
+  { value: "hr", label: "Hrvatski" },
+];
+
+const changeLanguage = (value) => {
+  $q.lang.set(value);
+  currentLang.value = value;
+  $q.localStorage.set("language", value);
+};
 </script>
+
+<style scoped>
+h1 {
+  color: #1976d2;
+}
+</style>
