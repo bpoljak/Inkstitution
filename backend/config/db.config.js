c83a6993453fb23f45da71a54c70ffa1de2eb42d
bpoljak <borna.poljak12@gmail.com>
2025-03-26 00:32:15 +0100

    Moved node modules to backend folder

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 try {
   const db = knex({
